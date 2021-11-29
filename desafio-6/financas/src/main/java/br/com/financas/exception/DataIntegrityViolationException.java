0e273dbdc42f76c91937ae9c2818843edab4fe47
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-11-29 11:36:13 -0300

    removing unused files

--- desafio-6/financas/src/main/java/br/com/financas/exception/DataIntegrityViolationException.java
+++
@@ -1,9 +0,0 @@
-package br.com.financas.exception;
-
-public class DataIntegrityViolationException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public DataIntegrityViolationException(String msg) { super(msg);}
-
-    public DataIntegrityViolationException(String msg, Throwable cause) {super(msg, cause);}
-}
