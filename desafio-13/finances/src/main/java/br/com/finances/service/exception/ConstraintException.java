cbc7533a0431b67f7c7d3f420bcf4efa6b9d716d
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-12-15 15:42:02 -0300

    desafio 13

---
+++ desafio-13/finances/src/main/java/br/com/finances/service/exception/ConstraintException.java
@@ -0,0 +1,12 @@
+package br.com.finances.service.exception;
+
+public class ConstraintException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public  ConstraintException(String msg) {super(msg);}
+
+    public ConstraintException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
+
